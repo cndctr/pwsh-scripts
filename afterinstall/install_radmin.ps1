615aa2c02eec8efa23be0202a23dab46b786228f
cndctr <conductor85@gmail.com>
2023-06-16 17:09:16 +0300

    Update install_radmin.ps1

--- afterinstall/install_radmin.ps1
+++ afterinstall/install_radmin.ps1
@@ -4,15 +4,13 @@ Write-Verbose -Message 'Installing Radmin' -Verbose
 
 Set-Location 'w:\soft\_utils\radmin\'
 Start-Process msiexec -ArgumentList '/i rserv35.msi /passive' -Wait
-$ServiceName = 'Rserver3'
-$ServiceStatus = (Get-Service -Name $ServiceName).Status
+$ServiceStatus = (Get-Service -Name RServer3).Status
 if ($ServiceStatus -eq 'Running') {
     Stop-Service RServer3
-}
-else {
     reg.exe import 'w:\soft\_utils\radmin\radmin_authNT.reg' # Reg-file sets access rights
     Copy-Item W:\soft\_utils\radmin\radmin_patch\* -Destination C:\Windows\SysWOW64\rserver30
 }
+
 Start-Service RServer3
 
 Write-Verbose -Message 'OK' -Verbose
